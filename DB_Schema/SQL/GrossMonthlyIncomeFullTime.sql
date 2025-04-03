209b9e15be85d8952c83dea143a382e8c9c7ead1
chingdavid <81205117+chingdavid@users.noreply.github.com>
2025-04-04 01:08:57 +0800

    Add files via upload

---
+++ DB_Schema/SQL/GrossMonthlyIncomeFullTime.sql
@@ -0,0 +1,17 @@
+-- Table: public.GrossMonthlyIncomeFullTime
+
+-- DROP TABLE IF EXISTS public."GrossMonthlyIncomeFullTime";
+
+CREATE TABLE IF NOT EXISTS public."GrossMonthlyIncomeFullTime"
+(
+    financial_year integer,
+    gross_monthly_income_including_employer_cpf integer,
+    increment_by_percent real
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."GrossMonthlyIncomeFullTime"
+    OWNER to postgres;
+
+GRANT ALL ON TABLE public."GrossMonthlyIncomeFullTime" TO postgres;
